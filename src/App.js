3f0d18431327fb0b4c07b3d9c558e13e8b2a40b7
lanbow93 <the.doctor1501@gmail.com>
2023-01-25 14:38:55 -0600

    Use effect implemented, still needs to be reloaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
-import { useState } from 'react';
+
 
 function App() {
 
-  const [pageDetails, setPageDetails] = useState({
-    homeUrl: "/users",
-    homeText: "Job Listings",
-    switchUrl: "/recruiters",
-    switchText: "Recruiters",
-    accessUrl: "/jsapplications",
-    accessText: "Submitted Applications"
-  })
-
+  
   return (
     <div className="App">
-      <Nav home={pageDetails.homeUrl} homeText={pageDetails.homeText} switchUrl={pageDetails.switchUrl} switchText={pageDetails.switchText} accessURL={pageDetails.accessUrl} accessText={pageDetails.accessText} switchDetails={setPageDetails} />
-      <Outlet testOut={pageDetails.accessText} />
+      <Nav />
+      <Outlet />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,64 @@
-import { Link, redirect } from "react-router-dom"
-
-let isSeeker = true
+import { Link } from "react-router-dom"
+import { useState, useEffect } from 'react';
 
 function Nav(props){
+    const [pageDetails, setPageDetails] = useState({
+        homeUrl: "/users",
+        homeText: "Job Listings",
+        switchUrl: "/recruiters",
+        switchText: "Recruiters",
+        accessUrl: "/jsapplications",
+        accessText: "Submitted Applications"
+      })
     
-     function changeUserType() {
-        if (isSeeker) {
-            isSeeker=false;
-            props.switchDetails({
-                homeUrl: "/recruiters",
-                homeText: "Posting Stats",
-                switchUrl: "/users",
-                switchText: "Job Seekers",
-                accessUrl: "/rapplications",
-                accessText: "Job Postings"
+      useEffect(() => {
+        const path = new URL(window.location).pathname
+        console.log("path", path) // just to make sure that last line worked
+       
+        switch(path){
+          case "/users":
+           setPageDetails({
+            homeUrl: "/users",
+            homeText: "Job Listings",
+            switchUrl: "/recruiters",
+            switchText: "Recruiters",
+            accessUrl: "/jsapplications",
+            accessText: "Submitted Applications"
+            
+    
+        })
+           break
+       
+          case "/recruiters":
+           setPageDetails({
+            homeUrl: "/recruiters",
+            homeText: "Posting Stats",
+            switchUrl: "/users",
+            switchText: "Job Seekers",
+            accessUrl: "/rapplications",
+            accessText: "Job Postings"
             })
-            return redirect("/recruiters")
-        } else {
-            isSeeker=true;
-            props.switchDetails({
-                homeUrl: "/users",
-                homeText: "Job Listings",
-                switchUrl: "/recruiters",
-                switchText: "Recruiters",
-                accessUrl: "/jsapplications",
-                accessText: "Submitted Applications"
-            })
-            return redirect("/users")
+           break
+       
+          default:
+            setPageDetails({
+              homeUrl: "/users",
+              homeText: "Job Listings",
+              switchUrl: "/recruiters",
+              switchText: "Recruiters",
+              accessUrl: "/jsapplications",
+              accessText: "Submitted Applications"
+          })
         }
-    }
-
+       
+       }, [])
+    
+    
+    
     return <nav>
-        <Link to={props.home}>{props.homeText}</Link>
-        <Link onClick={changeUserType} to={props.switchUrl}>{props.switchText}</Link>
-        <Link to={props.accessURL}>{props.accessText}</Link>
+        <Link to={pageDetails.homeUrl}>{pageDetails.homeText}</Link>
+        <Link to={pageDetails.switchUrl} >{pageDetails.switchText}</Link>
+        <Link to={pageDetails.accessUrl}>{pageDetails.accessText}</Link>
     </nav>
 }
 
